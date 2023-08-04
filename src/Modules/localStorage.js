8f3fdb10e7c6150ef1c5fab56265108577e1748b
hamidazim321 <129533107+hamidazim321@users.noreply.github.com>
2023-08-04 21:34:59 +0500

    Edit .gitignore file

---
+++ src/Modules/localStorage.js
@@ -0,0 +1,27 @@
+const getMyLikes = () => {
+  const likes = JSON.parse(localStorage.getItem('myLikes')) || []
+  return likes
+}
+
+const saveMyLikes = (id) => {
+  const likes = getMyLikes()
+  const newLike = {
+    id: id
+  }
+  likes.push(newLike)
+  localStorage.setItem('myLikes', JSON.stringify(likes))
+}
+
+const highlightMyLikes = () => {
+  const myLikes = getMyLikes()
+  const meals = document.querySelectorAll('.meal-card')
+
+  meals.forEach(meal => {
+    if (myLikes.find(obj => obj.id === meal.id)){
+      const likeButton = meal.querySelector('.likeButton')
+      likeButton.style.color = 'red'
+    }
+  })
+}
+
+export {highlightMyLikes, saveMyLikes, getMyLikes}
